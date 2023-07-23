d54e632b2a8e8420b53fef7c2ce6a07bbe65578d
Guillermo Rodriguez <guillerg0101@gmail.com>
2023-07-23 14:04:57 -0400

    agrege doc y termine el crud completo de eventos

--- controllers/events.js
+++ controllers/events.js
@@ -30,7 +30,7 @@ const crearEventos = async (req,res=response ) =>{
 
 console.log(req.body);
 const  evento = new Evento (req.body)
-const{tiempos,user} = req.body
+
 try{
 
     evento.user=req.uid
@@ -57,26 +57,112 @@ res.status(201).json({
 }
 
 
-const actualizarEvento = (req,res=response ) =>{
 
 
-    res.json({
 
-        ok:true,
-        msg:'actualizarEvento'
+
+
+
+
+
+
+
+
+const actualizarEvento = async (req,res=response ) =>{
+    const eventoId = req.params.id
+    const {uid}  = req.uid
+    const {tiempos,user} = req.body
+try{
+const evento =await Evento.findById(eventoId)
+if(!evento){
+
+    res.status(404).json({
+        ok:false,
+        msg:'Evento no existe'
     })
+}
+
+// if(evento.user.toString() !== uid){
+
+
+//     return res.status(401).json({
+//         ok:false,
+//         msg:'No tienes privilegio para editar'
+//     })
+// }
+const nuevoEvento = {
+    tiempos,
+    user
+}
+const eventoActualizado = await Evento.findByIdAndUpdate(eventoId,nuevoEvento)
+
+res.json({
+
+    ok:true,
+    msg:'actualizarEvento',
+    evento:eventoActualizado
+})
+
+}catch(error){
+    console.log(error);
+    return res.status(500).json({
+        ok:false,
+        msg:'Hable con el administrador'
+    })}
+
+
+
+
+
+
+
+
+
+
+   
 
 }
 
 
-const eliminarEvento = (req,res=response ) =>{
+const eliminarEvento = async (req,res=response ) =>{
 
 
-    res.json({
+   
+    const eventoId = req.params.id
+    const {uid}  = req.uid
+  
+try{
+const evento =await Evento.findById(eventoId)
+if(!evento){
 
-        ok:true,
-        msg:'eliminarEvento'
+    res.status(404).json({
+        ok:false,
+        msg:'Evento no existe'
     })
+}
+
+
+const eventoActualizado = await Evento.findByIdAndDelete(eventoId)
+
+res.json({
+
+    ok:true,
+    msg:'eliminarEvento',
+    evento:eventoActualizado
+})
+
+}catch(error){
+    console.log(error);
+    return res.status(500).json({
+        ok:false,
+        msg:'Hable con el administrador'
+    })}
+
+
+
+
+
+
 
 }
 
