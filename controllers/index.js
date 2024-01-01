2c24117113339ad7a750c6ad5182420ba4bb19e1
Guillermo Enrique Rodríguez Galban <93445026+guillerg01@users.noreply.github.com>
2024-01-01 16:56:24 -0500

    update new bd

---
+++ controllers/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+require("dotenv").config();
+const {dbConnection} = require('./db/config')
+const cors = require('cors')
+
+
+
+// crear servidor
+const app = express();
+
+//cors
+app.use(cors())
+
+
+
+//directorio publico
+app.use(express.static("public"));
+
+
+//bd
+dbConnection();
+
+//lectura y parseo del boddy
+app.use(express.json())
+
+//RUTAS
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/events",require("./routes/events"))
+app.use("/api/tiempos",require("./routes/tiempo"))
+
+//escuchar peticiones
+app.listen(process.env.PORT, () => {
+  console.log("server en 4000");
+});
